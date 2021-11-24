1a6b3c7c19fc3eeb398102608242b4f2407f1744
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2021-11-24 11:22:07 -0500

    chore: init commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "fundamentals.h"
+#include "manipulating.h"
+#include "converting.h"
+#include "tokenizing.h"
+
+int main() {
+	fundamentals();
+	manipulating();
+	converting();
+	tokenizing();
+	return 0;
+}
